7955acda7c74d884c65cd31204e18e37c7c4e240
Kiritoz <kiritoz@mail.ru>
2023-05-04 15:11:55 +0300

    New string

--- src/Practice/practice.java
+++ src/Practice/practice.java
@@ -7,6 +7,9 @@ public class practice {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         Random random = new Random();
-
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        int c = a + b;
+        System.out.println(c);
     }
 }
