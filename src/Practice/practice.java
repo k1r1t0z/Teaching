1159b5dcb2a9f1622130fee61f2ddb270bc3ab8a
Kiritoz <kiritoz@mail.ru>
2023-05-04 16:53:34 +0300

    Таблица умножения на 10

--- src/Practice/practice.java
+++ src/Practice/practice.java
@@ -7,9 +7,21 @@ public class practice {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         Random random = new Random();
+        /*System.out.print("Введите первое число: ");
         int a = scanner.nextInt();
+        System.out.print("Введите второе число: ");
         int b = scanner.nextInt();
-        int c = a + b;
-        System.out.println(c);
+        System.out.println(a + " + " + b + " = " + (a + b));
+        System.out.println(a + " - " + b + " = " + (a - b));
+        System.out.println(a + " * " + b + " = " + (a * b));
+        System.out.println(a + " / " + b + " = " + (a / b));*/
+
+        int a = scanner.nextInt();
+        int b;
+        for (int i = 1; i <= 10; i++) {
+            b = a * i;
+            System.out.println(a + " х " + i + " = "+ b);
+        }
+
     }
 }
