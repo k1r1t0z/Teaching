d157b296a02d9c809aa663d713a09b7eb34a1525
Kiritoz <kiritoz@mail.ru>
2023-05-04 15:07:36 +0300

    New string

--- src/Practice/practice.java
+++ src/Practice/practice.java
@@ -1,4 +1,12 @@
 package Practice;
 
+import java.util.Random;
+import java.util.Scanner;
+
 public class practice {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Random random = new Random();
+
+    }
 }
