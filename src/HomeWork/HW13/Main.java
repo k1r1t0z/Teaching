57e6880bbcbbb071a85d2885bc5f0908c425f975
Kiritoz <kiritoz@mail.ru>
2023-06-07 19:17:52 +0300

    HW13 Done1

--- src/HomeWork/HW13/Main.java
+++ src/HomeWork/HW13/Main.java
@@ -4,9 +4,7 @@ public class Main {
     public static void main(String[] args) {
         try {
             UserInfo.info("kiritoz@mail.ru", "krrrtZ23415", "krrrtZ23415");
-        } catch (WrongLoginException e) {
-            throw new RuntimeException(e);
-        } catch (WrongPasswordException e) {
+        } catch (WrongLoginException | WrongPasswordException e) {
             throw new RuntimeException(e);
         }
     }
